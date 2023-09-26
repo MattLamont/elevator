3c251727df287e55e993c61ee4c7913405a25842
Matt <matthewlamont454@gmail.com>
2023-09-26 15:19:04 -0600

    init commit

---
+++ src/main/java/elevator/Application.java
@@ -0,0 +1,11 @@
+package elevator;
+
+import elevator.Elevator;
+
+public class Application {
+    public static void main(String[] args){
+        
+        Elevator elevator = new Elevator();
+    }
+}
+
